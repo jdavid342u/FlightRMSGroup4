52f43564c7bd7408607f60873a90624ae53d2daa
jdavid342u <156380215+jdavid342u@users.noreply.github.com>
2024-03-09 18:50:02 -0700

    Update Reservation.cs

--- FlightRMSGroup4/Reservation.cs
+++ FlightRMSGroup4/Reservation.cs
@@ -14,12 +14,13 @@ namespace FlightRMSGroup4
         public string Citizenship { get; set; }
         public bool IsActive { get; set; }
 
-        public Reservation(string FlightCode, string Name, string Citizenship)
+        public Reservation(string ReservationCode, string FlightCode, string Name, string Citizenship)
         {
-            this.ReservationCode = null;
+            this.ReservationCode = ReservationCode;
             this.FlightCode = FlightCode;
             this.Name = Name;
             this.Citizenship = Citizenship;
+            this.IsActive = true; /* Active by default */
         }
     }
 }
